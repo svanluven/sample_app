78173d32219e04c3e82f69de09642f9c3c14defc
Svetlana Van Luven <svanluven@gmail.com>
2012-01-19 14:33:15 -0800

    Added Help

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -20,12 +20,34 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
+    it "should have the right title" do
+        get 'contact'
+        response.should have_selector( "title", :content => "Ruby on Rails Tutorial Sample App | Contact" )
+    end
   end
 
-describe "GET 'about'" do
+  describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
+    end
+
+    it "should have the right title" do
+        get 'about'
+        response.should have_selector( "title", :content => "Ruby on Rails Tutorial Sample App | About" )
+    end
+  end
+
+describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+        get 'help'
+        response.should have_selector( "title", :content => "Ruby on Rails Tutorial Sample App | Help" )
     end
   end
 
